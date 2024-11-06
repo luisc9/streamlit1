d79f965432270dbf3aee3579b3e91c7644e6abf7
luisc9 <40743595+luisc9@users.noreply.github.com>
2024-11-06 20:05:25 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,16 @@
 import streamlit as st
 import pandas as pd
+from sklearn.datasets import load_iris
+
+data = load_iris(as_frame=True)
+df =data.frame
+
+
 
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
+
+st.dataframe(df)
