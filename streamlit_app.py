ab96534e84ac2e975a8e9946d7d020c31e1e9ad4
luisc9 <luis.carraud@gmail.com>
2024-11-06 20:44:40 -0300

    added graph

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.datasets import load_wine
+import matplotlib.pyplot as plt
 
 # data = load_iris(as_frame=True)
 data = load_wine(as_frame=True)
@@ -19,3 +20,20 @@ st.write("Here is the wine dataframe")
 st.dataframe(df)
 
 st.write("Another change")
+
+st.write("Let's see the description")
+st.write(df.describe())
+
+
+# st.write("A graph")
+
+# # Plot relationship
+# plt.figure(figsize=(8,4))
+# categories = df["TARGET"].unique()
+# values = df_train["TARGET"].value_counts()
+# plt.bar(categories, values)
+# plt.title('TARGET variable distribution')
+# plt.xlabel('Values')
+# plt.ylabel("Count")
+# plt.xticks(categories)
+# plt.show()
