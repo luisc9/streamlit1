f2c5ad162a55a69e77fd9de1fd750a29c882af88
luisc9 <luis.carraud@gmail.com>
2024-11-07 13:11:59 -0300

    graphs again

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.datasets import load_wine
-# import matplotlib.pyplot as plt
-# import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
 
 # data = load_iris(as_frame=True)
 data = load_wine(as_frame=True)
@@ -24,14 +24,14 @@ st.write("Let's see the description")
 st.write(df.describe())
 
 
-# st.write("A graph")
+st.write("A graph")
 
-# plt.figure(figsize=(10, 6)) 
-# # Graficar los valores de la columna 'alcohol' 
-# plt.plot(df.index, df['alcohol'], marker='o', linestyle='-', color='b') 
-# # Añadir etiquetas y título 
-# plt.xlabel('Índice') 
-# plt.ylabel('Alcohol') 
-# plt.title('Valores de Alcohol en el Dataset de Vino')
+plt.figure(figsize=(10, 6)) 
+# Graficar los valores de la columna 'alcohol' 
+plt.plot(df.index, df['alcohol'], marker='o', linestyle='-', color='b') 
+# Añadir etiquetas y título 
+plt.xlabel('Índice') 
+plt.ylabel('Alcohol') 
+plt.title('Valores de Alcohol en el Dataset de Vino')
 
-# st.pyplot(plt)
+st.pyplot(plt)
